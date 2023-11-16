45ddf045016d151feb4f7cfce2e8298a5fea85d9
Giselle Pereira <56452652+giselle-lab@users.noreply.github.com>
2023-11-16 18:01:07 -0300

    ultima tabela testada

--- init/tagtable.sql
+++ init/tagtable.sql
@@ -1,28 +1,25 @@
--- @Table: tagtable
--- @Description: Table to store tags for BOCA entities
--- @Columns: tagid, entitytype, entityid, tagname, tagvalue, updatetime
--- @Constraints: tag_pkey (primary key), tag_index (index)
--- @Inserts: Data for tagtable
--- @DropTableIfExists
-DROP TABLE IF EXISTS tagtable;
-CREATE TABLE tagtable (
-    tagid SERIAL PRIMARY KEY,
-    entitytype VARCHAR(50) NOT NULL,
-    entityid VARCHAR(50) NOT NULL,
-    tagname VARCHAR(50) NOT NULL,
-    tagvalue VARCHAR(50),
-    updatetime INT DEFAULT EXTRACT(epoch FROM now()) NOT NULL,
-    CONSTRAINT tag_index UNIQUE (entitytype, entityid, tagname)
-);
+-- Adminer 4.8.1 PostgreSQL 14.10 dump
 
--- Example data for tagtable
--- @InsertData
-INSERT INTO tagtable (entitytype, entityid, tagname, tagvalue, updatetime) VALUES
-('problem', '2006', 'group', 'silberschatz', 1651674058),
-('problem', '2006', 'level', 'easy', 1651674058),
-('problem', '2006', 'domain', 'basic select', 1651674058),
-('problem', '2006', 'lang', 'relax', 1651674058),
-('site/user', '1/1001', 'working', 'atividade 01', 1651674058);
--- ... (other rows)
+DROP TABLE IF EXISTS "tagtable";
+DROP SEQUENCE IF EXISTS tagtable_tagid_seq;
+CREATE SEQUENCE tagtable_tagid_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
 
--- @EndOfFile
+CREATE TABLE "public"."tagtable" (
+    "tag_id" integer DEFAULT nextval('tagtable_tagid_seq') NOT NULL,
+    "entity_type" character varying(50) NOT NULL,
+    "entity_id" character varying(50) NOT NULL,
+    "tag_name" character varying(50) NOT NULL,
+    "tag_value" character varying(50),
+    "updatetime" integer DEFAULT EXTRACT(epoch FROM now()) NOT NULL,
+    CONSTRAINT "tag_index" UNIQUE ("entity_type", "entity_id", "tag_name"),
+    CONSTRAINT "tagtable_pkey" PRIMARY KEY ("tag_id")
+) WITH (oids = false);
+
+INSERT INTO "tagtable" ("tag_id", "entity_type", "entity_id", "tag_name", "tag_value", "updatetime") VALUES
+(2,	'problem',	'2006',	'level',	'easy',	1651674058),
+(3,	'problem',	'2006',	'domain',	'basic select',	1651674058),
+(4,	'problem',	'2006',	'lang',	'relax',	1651674058),
+(5,	'site/user',	'1/1001',	'working',	'atividade 01',	1651674058),
+(1,	'contest',	'2006',	'group',	'silberschatz',	1651674058);
+
+-- 2023-11-16 21:00:53.121478+00
